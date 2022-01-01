a7de900f97f4a17a57c6e03ec372b3c4c6593dac
Sarthak Poshattiwar <sarthak.poshattiwar@gmail.com>
2021-12-31 20:11:43 -0800

    commit on master

--- force-app/main/default/lwc/bool/bool.js
+++ force-app/main/default/lwc/bool/bool.js
@@ -2,4 +2,18 @@ import { LightningElement, api } from 'lwc';
 
 export default class Bool extends LightningElement {
     @api show = false;
+
+    constructor(){
+        super();
+        console.log('Constructor on child is called');
+    }
+    connectedCallback(){
+        console.log('Connected callback on child is called');
+    }
+    renderedCallback(){
+        console.log('renderredcallback on child is called');
+    }
+    disconnectedCallback(){
+        console.log('disconnected child callback');
+    }
 }
--- force-app/main/default/lwc/boolParent/boolParent.js
+++ force-app/main/default/lwc/boolParent/boolParent.js
@@ -1,3 +1,17 @@
 import { LightningElement } from 'lwc';
 
-export default class BoolParent extends LightningElement {}
+export default class BoolParent extends LightningElement {
+    constructor(){
+        super();
+        console.log('Constructor on parent is called');
+    }
+    connectedCallback(){
+        console.log('Connected callback on parent is called');
+    }
+    renderedCallback(){
+        console.log('renderredcallback on parent is called');
+    }
+    disconnectedCallback(){
+        console.log('disconnectedCallback on parent is called');
+    }
+}
